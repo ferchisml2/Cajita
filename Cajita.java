ad51c69f04a36e94e018fc310a445688d05a9929
ferchisml2 <20243rd003@utez.edu.mx>
2025-05-09 12:34:18 -0600

    Si funciona

---
+++ Cajita.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class Cajita {
+    public static void main(String[] args) {
+        String nombreUsuario = JOptionPane.showInputDialog ("Ingresa tu nombre");
+        System.out.println("Tu nombre es: "+nombreUsuario);
+
+    }
+}
+
