4cfd9c2b2cb87ec50be4d500a8781d1ce3b7b2ab
ferchisml2 <20243rd003@utez.edu.mx>
2025-05-10 16:52:11 -0600

    Este su funciona

--- Cajita.java
+++ Cajita.java
@@ -4,6 +4,20 @@ public class Cajita {
         String nombreUsuario = JOptionPane.showInputDialog ("Ingresa tu nombre");
         System.out.println("Tu nombre es: "+nombreUsuario);
 
+        String numero1 = JOptionPane.showInputDialog ("Ingresa un numero");
+        String numero2 = JOptionPane.showInputDialog ("Ingresa un numero");
+
+        int numeroEntero1 = Integer.parseInt(numero1);
+        int numeroEntero2 = Integer.parseInt(numero2);
+
+        String numero3 = JOptionPane.showInputDialog ("Ingresa un numero");
+        String numero4 = JOptionPane.showInputDialog ("Ingresa un numero");
+
+        double numeroDecimal1 = Double.parseDouble(numero3;)  //Casting es para la conversion de numeros
+        double numeroDecimal2 = Double.parseDouble(numero4;)
+
+
+
     }
 }
 
